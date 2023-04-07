b80c172dc3e3b25b708baa7fc1bf4e3606783409
jhtanjim~ <jh.tanjim.4@gmail.com>
2023-04-08 00:29:06 +0600

    useState done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css'
 import { useLoaderData } from 'react-router-dom';
+import TShirt from '../T-Shirt/TShirt';
+import Cart from '../Cart/Cart';
+
 const Home = () => {
 
     const tShirts = useLoaderData();
-    console.log(tShirts);
+    // console.log(tShirts);
+    // order calculation show
+    const [cart, setCart] = useState([])
+
+
+    const handleAddToCart = tShirt => {
+        console.log(tShirt);
+    }
     return (
 
-        <div>
-            <h2>This is Home {tShirts.length} </h2>
+        <div className='home-container'>
+            <div className="tshirts-container">
+                {
+                    tShirts.map(tShirt => <TShirt
+
+                        key={tShirt._id}
+                        tShirt={tShirt}
+                        handleAddToCart={handleAddToCart}
+                    ></TShirt>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
